329d82487f8ceebbb4fafad72093af4061b2ca66
Winnie Ho <winnie.ho@makeitsocial.com>
2017-05-16 12:00:26 +0100

    test

---
+++ YDKJS/types_and_grammar.js
@@ -0,0 +1,25 @@
+console.log(typeof undefined);
+console.log(typeof true);
+console.log(typeof 42);
+console.log(typeof "Hello");
+console.log(typeof {first: "Winie"});
+console.log(typeof null);
+console.log(typeof [1, 2, 3]);
+
+
+function myFunction(name, lastname, dob){
+    console.log(name);
+}
+
+myFunction("Winnie", "Ho", "01010000")
+myFunction.length;
+
+var a;
+var b = 42;
+var c;
+
+b=c;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
